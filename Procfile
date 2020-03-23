3b3d6a432ad43ece66647de079c31b96aeb3fc79
Antonio <antoniodiasabc@gmail.com>
2020-03-23 18:09:50 -0300

    testes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python t9.py
+web: python t8.py
--- t8.py
+++ t8.py
@@ -7,24 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def nao_entre_em_panico():
 
-    limite = 99
 
-    c = 1
-    p = 1
-    numero = 3
+    primos = "Tudo vai dar certo!"
 
-    primos = "2,"
-
-    while p < limite:
-        ehprimo = 1
-        for i in range(2, numero):
-            if numero % i == 0:
-                ehprimo = 0
-                break
-        if (ehprimo):
-            primos = primos + str(numero) + ","
-            p += 1
-        numero+=1
 
     return primos
 
